e78de0c2dce49f55da569c3364462f633cf277b5
marcove3 <marcove3@gmail.com>
2025-08-26 21:35:36 -0400

    update sass and move auth.ts to lib folder

--- lib/strava.ts
+++ lib/strava.ts
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import authOptions from "@/app/lib/auth";
+import authOptions from "@/lib/auth";
 
 class StravaApiError extends Error {
   status: number;
