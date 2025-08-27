732aefcd122593a534a635709cb4cfc6d3bb7e08
marcove3 <marcove3@gmail.com>
2025-08-26 20:56:21 -0400

    update css to avoid vercel errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,7 +28,7 @@ export default async function RootLayout({
   const session = await getServerSession();
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body>
         <Providers session={session}>
           <NavBar />
           <main>{children}</main>
